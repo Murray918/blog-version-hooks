ec0e32172c7dcadcf159a3435f57e1e211f972a4
Murray918 <darbyguitars@gmail.com>
2019-07-13 20:01:45 -0500

    connecting the Main component

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,1 +1,16 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+const Main = props => {
+	return (
+		<section>
+			<h1>hello there this is the first part of the blogs right here</h1>
+		</section>
+	)
+}
+
+export default Main
+
+Main.propTypes = {
+	isLoggedIn: PropTypes.bool
+}
